a0afaae6278b2621e85789370918f29b956dd0a6
Tim Jones <timjonez@protonmail.com>
2020-07-29 09:11:54 -0400

    add initial files

---
+++ project/project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'h4d##2y&i9(+a=@_q(@n2em%i@+s%lg7#5*r1-+3=bbe7g=yry'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
